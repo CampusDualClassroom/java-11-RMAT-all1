741288bfccc6b5ff6be2efc869c83f6cf8180637
RMAT-all1 <antithelemon@gmail.com>
2024-11-21 14:31:25 +0100

    Ejercicio 11 RESUELTO

--- src/main/java/com/campusdual/classroom/TVRemote.java
+++ src/main/java/com/campusdual/classroom/TVRemote.java
@@ -2,4 +2,69 @@ package com.campusdual.classroom;
 
 public class TVRemote {
 
+    boolean on;
+    int channel;
+    int volume;
+    String color;
+
+    // Constructor
+    public TVRemote(String color) {
+        this.color = color;
+        this.channel = 0;
+        this.volume = 20;
+    }
+
+    // Métodos para encender y apagar la TV
+    public boolean turnOn() {
+        this.on = true;
+        return this.on;
+    }
+
+    public boolean turnOff() {
+        this.on = false;
+        return this.on;
+    }
+
+    // Métodos para gestionar el canal
+    public void channelUp() {
+        this.channel++;
+    }
+
+    public void channelDown() {
+        if (this.checkMinValue(this.channel)) {
+            if (this.channel > 0) {
+                this.channel--;
+            }
+        }
+    }
+
+    // Métodos para gestionar el volumen
+    public void volumeUp() {
+        this.volume++;
+    }
+
+    public void volumeDown() {
+        if (this.checkMinValue(this.volume)) {
+            if (this.volume > 0) {
+                this.volume--;
+
+            }
+        }
+    }
+
+    // Méodo para obtener el color del mando
+    public String getColor() {
+        return this.color;
+    }
+
+    private boolean checkMinValue(int value) {
+        if (value == 0) {
+        } else {
+        }
+        return true;
+    }
 }
+
+
+
+
